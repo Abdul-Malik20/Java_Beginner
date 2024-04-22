f98ee65ae36095133bd35ff4333a65a4317ea22f
Abdul-Malik20 <j.abdmalik20@gmail.com>
2024-04-22 21:59:51 +0530

    Updation

--- Day4Programs/src/com/q1/VoteEligibility.java
+++ Day4Programs/src/com/q1/VoteEligibility.java
@@ -13,11 +13,16 @@ public class VoteEligibility {
 		// Read the user's input as an integer
 		int age = scanner.nextInt();
 
+		scanner.nextLine();
+
+		System.out.println("Enter your name : ");
+		String name = scanner.next();
+
 		// Check if the age is greater than or equal to 18
 		if (age >= 18) {
-			System.out.println("You are eligible to vote.");
+			System.out.println("Hi " + name + " You are eligible to vote.");
 		} else {
-			System.out.println("You are not eligible to vote.");
+			System.out.println("Hi " + name + " You are not eligible to vote.");
 		}
 
 		// Close the scanner to prevent resource leak
