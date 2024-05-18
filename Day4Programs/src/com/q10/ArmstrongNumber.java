d174c6a721b6b98cb6da630ae797758418a89e1d
Abdul-Malik20 <j.abdmalik20@gmail.com>
2024-05-18 10:35:51 +0530

    no changes

--- Day4Programs/src/com/q10/ArmstrongNumber.java
+++ Day4Programs/src/com/q10/ArmstrongNumber.java
@@ -5,23 +5,25 @@ package com.q10;
 public class ArmstrongNumber {
 	public static void main(String[] args) {
 		// Create a Scanner object to read input from the user
-		//Scanner scanner = new Scanner(System.in);
+		// Scanner scanner = new Scanner(System.in);
 
 		// Prompt the user to enter a number
-		//System.out.print("Enter a number: ");
+		// System.out.print("Enter a number: ");
 
 		// Read the user's input as an integer
 		int number = 153;
-int n1 = number;
+		int n1 = number;
 		// Check if the number is an Armstrong number
 		int armNumber = 0;
 		do {
 			int remainder = number % 10;
 			armNumber = armNumber + (remainder * remainder * remainder);
-			number = number/10;
+			number = number / 10;
 		} while (number > 0);
-		if(n1==number) {
-			System.out.println("Yes this Armstrong");
-		}else {System.out.println("no");}
+		if (n1 == number) {
+			System.out.println("Yes this is Armstrong");
+		} else {
+			System.out.println("no");
+		}
 	}
 }
