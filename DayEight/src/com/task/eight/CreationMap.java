01d25547af5ef2b52a786f8764e7355dec14d49f
Abdul-Malik20 <j.abdmalik20@gmail.com>
2024-05-18 09:36:01 +0530

    no change

--- DayEight/src/com/task/eight/CreationMap.java
+++ DayEight/src/com/task/eight/CreationMap.java
@@ -1,11 +1,11 @@
 package com.task.eight;
 
 import java.util.Map;
-import java.util.Hashtable;
+import java.util.HashMap;
 
 public class CreationMap {
 	public static void main(String[] args) {
-		Map<String, String> List = new Hashtable<>();
+		Map<String, String> List = new HashMap<>();
 		List.put("Selenium", "Java");
 		List.put("bat", "oracle");
 		List.put("cat", "framework");
