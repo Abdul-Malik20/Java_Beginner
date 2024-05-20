bd7b1e6e1d5926a7ab8200ad26c5514b77a05f01
Abdul-Malik20 <j.abdmalik20@gmail.com>
2024-05-20 19:34:33 +0530

    Day 11 - Exception Handling

---
+++ Day10Testing/src/com/org/ten/Test3.java
@@ -0,0 +1,16 @@
+package com.org.ten;
+
+public class Test3 {
+	public static void main(String[] args) {
+		try {
+			System.out.printf("1");
+			int data = 5 / 0;
+		} catch (ArithmeticException e) {
+			System.out.printf("2");
+			System.exit(0);
+		} finally {
+			System.out.printf("3");
+		}
+		System.out.printf("4");
+	}
+}
